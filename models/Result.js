fd67cec47da4b3c1e731c475366c6280ae3d3ab4
Soul Evans <soulevans07@gmail.com>
2018-11-13 02:44:35 +0100

    statistic models changed

--- models/Result.js
+++ models/Result.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+const Schema = require('mongoose').Schema;
 
 const resultSchema = new mongoose.Schema({
   user: { type: Schema.Types.ObjectId, ref: 'User' },
-  game: { type: Schema.Types.ObjectId, ref: 'Game' },
+  word: { type: Schema.Types.ObjectId, ref: 'Word' },
   all: Number,
   correct: Number
 });
