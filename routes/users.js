2c8439a7f83fa9867a503196c4f1538f3881778d
Soul Evans <soulevans07@gmail.com>
2018-11-23 13:26:26 +0100

    added missing user routes

---
+++ routes/users.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
+
+const router = express.Router();
+
+router.get('/list',
+  authController.authenticate,
+  authController.requireAdmin,
+  userController.list
+);
+
+router.post('/:id/update',
+  authController.authenticate,
+  authController.requireAdmin,
+  userController.update
+);
+
+router.delete('/:id/delete',
+  authController.authenticate,
+  authController.requireAdmin,
+  userController.delete
+);
+
+router.get('/:id',
+  authController.authenticate,
+  authController.requireAdmin,
+  userController.get
+);
+
+
+module.exports = router;
