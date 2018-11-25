628fc58e04ba8593691ae0166d818f758a4027eb
Soul Evans <soulevans07@gmail.com>
2018-11-25 22:04:45 +0100

    added default users in seeds

--- importAll.sh
+++ importAll.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-uri="mongodb://localhost/linguo_dev"
-# uri="mongodb://szar_hf:4hqgyN6mYcvu0jRH@learning-shard-00-00-ni0dr.mongodb.net:27017,learning-shard-00-01-ni0dr.mongodb.net:27017,learning-shard-00-02-ni0dr.mongodb.net:27017/staging?ssl=true&replicaSet=learning-shard-0&authSource=admin&retryWrites=true"
+# uri="mongodb://localhost/linguo"
+uri="mongodb://szar_hf:4hqgyN6mYcvu0jRH@learning-shard-00-00-ni0dr.mongodb.net:27017,learning-shard-00-01-ni0dr.mongodb.net:27017,learning-shard-00-02-ni0dr.mongodb.net:27017/staging?ssl=true&replicaSet=learning-shard-0&authSource=admin&retryWrites=true"
 collections=("users" "levels" "words" "lessons" "games" "questions" "answers")
 
 for c in "${collections[@]}"
